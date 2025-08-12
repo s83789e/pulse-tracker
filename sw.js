134701dd0de21c25968c321611a6740b2c8df9b0
s83789e <soniafiala@googlemail.com>
2025-08-12 20:40:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 self.addEventListener('install', e=>{
   self.skipWaiting();
-  e.waitUntil(caches.open('pulse-cache-v433').then(c=> c.addAll(['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'])));
+  e.waitUntil(caches.open('pulse-v434').then(c=>c.addAll([
+    './','./index.html','./style.css','./script.js',
+    './manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
+  ])));
 });
 self.addEventListener('activate', e=> self.clients.claim());
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
+self.addEventListener('fetch', e=> e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))));
